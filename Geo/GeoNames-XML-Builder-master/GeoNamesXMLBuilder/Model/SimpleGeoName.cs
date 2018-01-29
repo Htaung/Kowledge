55ecf3977a7f3bba9bf6674411df8c4530265728
aungaung <aungtrt@gmail.com>
2018-01-29 14:54:19 +0630

    kd-tree

---
+++ Geo/GeoNames-XML-Builder-master/GeoNamesXMLBuilder/Model/SimpleGeoName.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenjaminSchroeter.GeoNames;
+
+namespace GeoNamesXMLBuilder.Model
+{
+    public class SimpleGeoName
+    {
+        public SimpleGeoName()
+        {
+            Children = new List<SimpleGeoName>();
+        }
+        public SimpleGeoName(Geoname geoname)
+        {
+            Id = geoname.Id;
+            Name = geoname.Name;
+            FCode = geoname.Fcode;
+            CountryCode = geoname.CountryCode;
+            
+            Children = new List<SimpleGeoName>();
+
+            LocalNameES = geoname.AlternateNames().Where(x => x.Language == "es").LastOrDefault().Name;
+            LocalNameEN = geoname.AlternateNames().Where(x => x.Language == "en").LastOrDefault().Name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<SimpleGeoName> Children { get; set; }
+        public string FCode { get; set; }
+        public string CountryCode { get; set; }
+
+        public string LocalNameEN { get; set; }
+        public string LocalNameES { get; set; }
+    }
+}
